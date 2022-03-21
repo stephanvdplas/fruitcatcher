2532d12c3be450ed223bc66d2bdbf7b186fa4c91
Stephan van der Plas <stephan@vdplas.net>
2022-03-21 12:05:56 +0100

    Terugveranderen van stenenregen naar fruitcatcher

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    speler.change(LedSpriteProperty.X, -1)
+    varken.change(LedSpriteProperty.X, -1)
 })
 input.onButtonPressed(Button.B, function () {
-    speler.change(LedSpriteProperty.X, 1)
+    varken.change(LedSpriteProperty.X, 1)
 })
 let fruit: game.LedSprite = null
-let speler: game.LedSprite = null
-speler = game.createSprite(2, 4)
+let varken: game.LedSprite = null
+varken = game.createSprite(2, 4)
 basic.forever(function () {
     fruit = game.createSprite(randint(0, 4), 0)
     for (let index = 0; index < 4; index++) {
         basic.pause(1000)
         fruit.change(LedSpriteProperty.Y, 1)
     }
-    if (fruit.isTouching(speler)) {
-        game.gameOver()
-    } else {
+    if (fruit.isTouching(varken)) {
         game.addScore(1)
         fruit.delete()
+    } else {
+        game.gameOver()
     }
 })
